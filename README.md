beef841ddd4cc99ef9187a0b6a3709b17a491c47
autom8ter <coleman.word@autom8ter.com>
2020-04-14 14:10:07 -0600

    geodb client

---
+++ README.md
@@ -0,0 +1,182 @@
+# geodb_go
+--
+    import "github.com/autom8ter/geodb-go"
+
+
+## Usage
+
+#### type Client
+
+```go
+type Client struct {
+}
+```
+
+Client is a geodb client
+
+#### func  NewClient
+
+```go
+func NewClient(ctx context.Context, cfg *ClientConfig) (*Client, error)
+```
+NewClient creates a new GeoDb client with the given Config
+
+#### func (Client) Delete
+
+```go
+func (c Client) Delete(ctx context.Context, r *api.DeleteRequest) (*api.DeleteResponse, error)
+```
+Delete - input: an array of object key strings to delete, output: none
+
+#### func (Client) Get
+
+```go
+func (c Client) Get(ctx context.Context, r *api.GetRequest) (*api.GetResponse, error)
+```
+Get - input: an array of object keys, output: returns an indexed map of current
+object details
+
+#### func (Client) GetKeys
+
+```go
+func (c Client) GetKeys(ctx context.Context, r *api.GetKeysRequest) (*api.GetKeysResponse, error)
+```
+GetKeys - input: none, output: returns all keys in database
+
+#### func (Client) GetPoint
+
+```go
+func (c Client) GetPoint(ctx context.Context, r *api.GetPointRequest) (*api.GetPointResponse, error)
+```
+GetPoint can be used to get an addresses latitude/longitude
+
+#### func (Client) GetPrefix
+
+```go
+func (c Client) GetPrefix(ctx context.Context, r *api.GetPrefixRequest) (*api.GetPrefixResponse, error)
+```
+GetPrefix - input: a prefix string, output: returns an indexed map of current
+object details with keys that have the given prefix
+
+#### func (Client) GetPrefixKeys
+
+```go
+func (c Client) GetPrefixKeys(ctx context.Context, r *api.GetPrefixKeysRequest) (*api.GetPrefixKeysResponse, error)
+```
+GetPrefixKeys - input: a prefix string, output: returns an array of of keys that
+have the given prefix
+
+#### func (Client) GetRegex
+
+```go
+func (c Client) GetRegex(ctx context.Context, r *api.GetRegexRequest) (*api.GetRegexResponse, error)
+```
+GetRegex - input: a regex string, output: returns an indexed map of current
+object details with keys that match the regex pattern
+
+#### func (Client) GetRegexKeys
+
+```go
+func (c Client) GetRegexKeys(ctx context.Context, r *api.GetRegexKeysRequest) (*api.GetRegexKeysResponse, error)
+```
+GetRegexKeys - input: a regex string, output: returns all keys in database that
+match the regex pattern
+
+#### func (Client) Ping
+
+```go
+func (c Client) Ping(ctx context.Context, r *api.PingRequest) (*api.PingResponse, error)
+```
+Ping - input: empty, output: returns ok if server is healthy.
+
+#### func (Client) ScanBound
+
+```go
+func (c Client) ScanBound(ctx context.Context, r *api.ScanBoundRequest) (*api.ScanBoundResponse, error)
+```
+ScanBound - input: a geolocation boundary, output: returns an indexed map of
+current object details that are within the boundary
+
+#### func (Client) ScanPrefixBound
+
+```go
+func (c Client) ScanPrefixBound(ctx context.Context, r *api.ScanPrefixBoundRequest) (*api.ScanPrefixBoundResponse, error)
+```
+ScanPrefexBound - input: a geolocation boundary, output: returns an indexed map
+of current object details that have keys that match the prefix and are within
+the boundary and
+
+#### func (Client) ScanRegexBound
+
+```go
+func (c Client) ScanRegexBound(ctx context.Context, r *api.ScanRegexBoundRequest) (*api.ScanRegexBoundResponse, error)
+```
+ScanRegexBound - input: a geolocation boundary, string-array of unique object
+ids(optional), output: returns an indexed map of current object details that
+have keys that match the regex and are within the boundary and
+
+#### func (Client) Set
+
+```go
+func (c Client) Set(ctx context.Context, r *api.SetRequest) (*api.SetResponse, error)
+```
+Set - input: an array of objects output: returns an indexed map of updated
+object details. objects are upserted in the order they are sent
+
+#### func (Client) Stream
+
+```go
+func (c Client) Stream(ctx context.Context, r *api.StreamRequest, handler StreamHandler) error
+```
+Stream - input: a clientID(optional) and an array of object keys(optional),
+output: a stream of object details for realtime, targeted object geolocation
+updates
+
+#### func (Client) StreamPrefix
+
+```go
+func (c Client) StreamPrefix(ctx context.Context, r *api.StreamPrefixRequest, handler StreamHandler) error
+```
+StreamPrefix - input: a clientID(optional) a prefix string, output: a stream of
+object details for realtime, targeted object geolocation updates that match the
+prefix pattern
+
+#### func (Client) StreamRegex
+
+```go
+func (c Client) StreamRegex(ctx context.Context, r *api.StreamRegexRequest, handler StreamHandler) error
+```
+StreamRegex - input: a clientID(optional) a regex string, output: a stream of
+object details for realtime, targeted object geolocation updates that match the
+regex pattern
+
+#### type ClientConfig
+
+```go
+type ClientConfig struct {
+	Host     string //geodb server url ex: localhost:8080
+	Password string //optional - only necessary if basic auth is enabled on geodb server
+	Metrics  bool   //register client side prometheus metrics interceptor
+	Retry    bool   //register client side retry interceptor
+}
+```
+
+ClientConfig holds configuration options for creating a Client
+
+#### type ObjectStreamGetter
+
+```go
+type ObjectStreamGetter interface {
+	GetObject() *api.ObjectDetail
+}
+```
+
+ObjectStreamGetter pulls object details off of a stream
+
+#### type StreamHandler
+
+```go
+type StreamHandler func(ctx context.Context, stream ObjectStreamGetter, err error)
+```
+
+StreamHandler executes logic on an object stream
---
+++ client.go
@@ -0,0 +1,197 @@
+//go:generate godocdown -o README.md
+
+package geodb_go
+
+import (
+	"context"
+	"fmt"
+	"github.com/autom8ter/geodb/gen/go/geodb"
+	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
+	"github.com/grpc-ecosystem/go-grpc-middleware/util/metautils"
+	"github.com/piotrkowalczuk/promgrpc/v3"
+	"github.com/prometheus/client_golang/prometheus"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+//Client is a geodb client
+type Client struct {
+	client api.GeoDBClient
+	authFn func(ctx context.Context) context.Context
+}
+
+//ClientConfig holds configuration options for creating a Client
+type ClientConfig struct {
+	Host     string //geodb server url ex: localhost:8080
+	Password string //optional - only necessary if basic auth is enabled on geodb server
+	Metrics  bool   //register client side prometheus metrics interceptor
+	Retry    bool   //register client side retry interceptor
+}
+
+//NewClient creates a new GeoDb client with the given Config
+func NewClient(ctx context.Context, cfg *ClientConfig) (*Client, error) {
+	unary := []grpc.UnaryClientInterceptor{}
+	stream := []grpc.StreamClientInterceptor{}
+	opts := []grpc.DialOption{}
+	if cfg.Metrics {
+		var promInterceptor = promgrpc.NewInterceptor(promgrpc.InterceptorOpts{})
+		if err := prometheus.DefaultRegisterer.Register(promInterceptor); err != nil {
+			return nil, err
+		}
+		unary = append(unary, promInterceptor.UnaryClient())
+		stream = append(stream, promInterceptor.StreamClient())
+		opts = append(opts, grpc.WithStatsHandler(promInterceptor))
+	}
+	if cfg.Retry {
+		unary = append(unary, grpc_retry.UnaryClientInterceptor())
+		stream = append(stream, grpc_retry.StreamClientInterceptor())
+	}
+	if len(unary) > 0 {
+		opts = append(opts, grpc.WithChainUnaryInterceptor(unary...))
+	}
+	if len(stream) > 0 {
+		opts = append(opts, grpc.WithChainStreamInterceptor(stream...))
+	}
+
+	conn, err := grpc.DialContext(ctx, cfg.Host, opts...)
+	if err != nil {
+		return nil, err
+	}
+	client := &Client{client: api.NewGeoDBClient(conn)}
+	if cfg.Password != "" {
+		client.authFn = func(ctx context.Context) context.Context {
+			md := metadata.Pairs("authorization", fmt.Sprintf("basic %v", cfg.Password))
+			return metautils.NiceMD(md).ToOutgoing(ctx)
+		}
+	} else {
+		client.authFn = func(ctx context.Context) context.Context {
+			return ctx
+		}
+	}
+	return client, nil
+}
+
+//ObjectStreamGetter pulls object details off of a stream
+type ObjectStreamGetter interface {
+	GetObject() *api.ObjectDetail
+}
+
+//StreamHandler executes logic on an object stream
+type StreamHandler func(ctx context.Context, stream ObjectStreamGetter, err error)
+
+//Ping - input: empty, output: returns ok if server is healthy.
+func (c Client) Ping(ctx context.Context, r *api.PingRequest) (*api.PingResponse, error) {
+	return c.client.Ping(c.authFn(ctx), r)
+}
+
+//Set - input: an array of objects output: returns an indexed map of updated object details.
+//objects are upserted in the order they are sent
+func (c Client) Set(ctx context.Context, r *api.SetRequest) (*api.SetResponse, error) {
+	return c.client.Set(c.authFn(ctx), r)
+}
+
+//Get - input: an array of object keys, output: returns an indexed map of current object details
+func (c Client) Get(ctx context.Context, r *api.GetRequest) (*api.GetResponse, error) {
+	return c.client.Get(c.authFn(ctx), r)
+}
+
+//GetRegex - input: a regex string, output: returns an indexed map of current object details with keys that match the regex pattern
+func (c Client) GetRegex(ctx context.Context, r *api.GetRegexRequest) (*api.GetRegexResponse, error) {
+	return c.client.GetRegex(c.authFn(ctx), r)
+}
+
+//GetPrefix - input: a prefix string, output: returns an indexed map of current object details with keys that have the given prefix
+func (c Client) GetPrefix(ctx context.Context, r *api.GetPrefixRequest) (*api.GetPrefixResponse, error) {
+	return c.client.GetPrefix(c.authFn(ctx), r)
+}
+
+//GetKeys -  input: none, output: returns all keys in database
+func (c Client) GetKeys(ctx context.Context, r *api.GetKeysRequest) (*api.GetKeysResponse, error) {
+	return c.client.GetKeys(c.authFn(ctx), r)
+}
+
+//GetRegexKeys -  input: a regex string, output: returns all keys in database that match the regex pattern
+func (c Client) GetRegexKeys(ctx context.Context, r *api.GetRegexKeysRequest) (*api.GetRegexKeysResponse, error) {
+	return c.client.GetRegexKeys(c.authFn(ctx), r)
+}
+
+//GetPrefixKeys - input: a prefix string, output: returns an array of of keys that have the given prefix
+func (c Client) GetPrefixKeys(ctx context.Context, r *api.GetPrefixKeysRequest) (*api.GetPrefixKeysResponse, error) {
+	return c.client.GetPrefixKeys(c.authFn(ctx), r)
+}
+
+//Delete -  input: an array of object key strings to delete, output: none
+func (c Client) Delete(ctx context.Context, r *api.DeleteRequest) (*api.DeleteResponse, error) {
+	return c.client.Delete(c.authFn(ctx), r)
+}
+
+//Stream -  input: a clientID(optional) and an array of object keys(optional),
+//output: a stream of object details for realtime, targeted object geolocation updates
+func (c Client) Stream(ctx context.Context, r *api.StreamRequest, handler StreamHandler) error {
+	client, err := c.client.Stream(c.authFn(ctx), r)
+	if err != nil {
+		return err
+	}
+	for {
+		if ctx.Err() != nil {
+			break
+		}
+		resp, err := client.Recv()
+		handler(ctx, resp, err)
+	}
+	return nil
+}
+
+//StreamRegex -  input: a clientID(optional) a regex string,
+//output: a stream of object details for realtime, targeted object geolocation updates that match the regex pattern
+func (c Client) StreamRegex(ctx context.Context, r *api.StreamRegexRequest, handler StreamHandler) error {
+	client, err := c.client.StreamRegex(c.authFn(ctx), r)
+	if err != nil {
+		return err
+	}
+	for {
+		if ctx.Err() != nil {
+			break
+		}
+		resp, err := client.Recv()
+		handler(ctx, resp, err)
+	}
+	return nil
+}
+
+//StreamPrefix -  input: a clientID(optional) a prefix string,
+//output: a stream of object details for realtime, targeted object geolocation updates that match the prefix pattern
+func (c Client) StreamPrefix(ctx context.Context, r *api.StreamPrefixRequest, handler StreamHandler) error {
+	client, err := c.client.StreamPrefix(c.authFn(ctx), r)
+	if err != nil {
+		return err
+	}
+	for {
+		if ctx.Err() != nil {
+			break
+		}
+		resp, err := client.Recv()
+		handler(ctx, resp, err)
+	}
+	return nil
+}
+
+//ScanBound -  input: a geolocation boundary, output: returns an indexed map of current object details that are within the boundary
+func (c Client) ScanBound(ctx context.Context, r *api.ScanBoundRequest) (*api.ScanBoundResponse, error) {
+	return c.client.ScanBound(c.authFn(ctx), r)
+}
+
+//ScanRegexBound -  input: a geolocation boundary, string-array of unique object ids(optional), output: returns an indexed map of current object details that have keys that match the regex and are within the boundary and
+func (c Client) ScanRegexBound(ctx context.Context, r *api.ScanRegexBoundRequest) (*api.ScanRegexBoundResponse, error) {
+	return c.client.ScanRegexBound(c.authFn(ctx), r)
+}
+
+//ScanPrefexBound -  input: a geolocation boundary, output: returns an indexed map of current object details that have keys that match the prefix and are within the boundary and
+func (c Client) ScanPrefixBound(ctx context.Context, r *api.ScanPrefixBoundRequest) (*api.ScanPrefixBoundResponse, error) {
+	return c.client.ScanPrefixBound(c.authFn(ctx), r)
+}
+
+//GetPoint can be used to get an addresses latitude/longitude
+func (c Client) GetPoint(ctx context.Context, r *api.GetPointRequest) (*api.GetPointResponse, error) {
+	return c.client.GetPoint(c.authFn(ctx), r)
+}
